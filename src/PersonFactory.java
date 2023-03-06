d33c752f195f8b2fcdee04885a35bb166f0e15cf
i-jonathan-hui <jonathanhui1230@gmail.com>
2023-03-06 13:15:36 +0800

    ff

--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class PersonFactory {
-
     private ArrayList<Person> allPersons;
 
     public PersonFactory() {
@@ -9,14 +8,33 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
-
+        for (int i=0; i<allPersons.size(); i++) {
+            String currentLastName = allPersons.get(i).getLastName();
+            if (person.getLastName().compareTo(currentLastName) == 0) {
+                allPersons.add(i, person);
+                break;
+            }
+            else if (person.getLastName().compareTo(currentLastName) < 0) {
+                allPersons.add(i, person);
+                break;
+            }
+        }
+        if (allPersons.size() <= 1) allPersons.add(person);
+        Collections.reverse(allPersons);
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> allUnder18 = new ArrayList<>();
+        for (int i=0; i<allPersons.size(); i++) {
+            if (allPersons.get(i).getAge() < 18) allUnder18.add(allPersons.get(i));
+        }
+        return allUnder18;
     }
 
     public boolean checkLastName(String lastName) {
-
+        for (int i=0; i<allPersons.size(); i++) {
+            if (allPersons.get(i).getLastName().equals(lastName)) return true;
+        }
+        return false;
     }
 }
